36b57b685d561cc358d32e47bb82d03e66b4dfa9
Navigoo <...>
2024-04-18 11:33:42 +0200

    2

--- server/server.js
+++ server/server.js
@@ -15,10 +15,35 @@ app.use(cors());
 
 //Metoder med axios
 //GET
+app.get('/cities', async (req, res) => {
+    try {
+        //vi testar att göra ett axios anrop till vår adress
+        const response = await axios.get('https://avancera.app/cities');
+        res.json(response.data);
+    } catch (error) {
+        //Om det uppstår ett fel, skicka tillbaka nedan som svar
+        res.status(500).json({ error: error.message });
+    }
+});
 
+//POST, vi skickar in en ny stad till avancera
+app.post('/cities', async (req, res) => {
+    try {
+        const { name, population } = req.body;
+        const response = await axios.post('https://avancera.app/cities', {
+            name,
+            population
+        });
+        //Skicka tillbaka det nya objektet som JSON
+        res.json(response.data);
+    } catch (error) {
+        //Om det uppstår ett fel, skicka tillbaka nedan som svar
+        res.status(500).json({ error: error.message });
+    }
+});
 
 //Lyssna på porten 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servern lyssnar på port ${PORT}`)
-})
+    console.log(`Servern lyssnar på port ${PORT}`);
+});
